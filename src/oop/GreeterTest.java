97f2e379f2383dd4e76b21ad52423f64adcfb66d
Colin <20szymbc@elmbrookstudents.org>
2018-10-19 13:08:05 -0500

    Greeter and GreeterTest

---
+++ src/oop/GreeterTest.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class GreeterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter bob = new Greeter();
+		System.out.println("age =" + bob.getAge());
+		bob.setAge(24);
+		System.out.println("age =" + bob.getAge());
+
+	}
+
+}
