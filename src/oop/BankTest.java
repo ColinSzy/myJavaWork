6c435a3c3f7cd195c5e6fe239ea53a8dd098e712
Colin <20szymbc@elmbrookstudents.org>
2018-10-24 08:38:26 -0500

    Bank

---
+++ src/oop/BankTest.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class BankTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Bank saving = new Bank();
+		Bank checking = new Bank();
+		
+		System.out.println("Acount Number: " + saving.getAcount() );
+		saving.deposit(100);
+		System.out.println("Saving Acount Total: " + saving.getMoney() );
+		checking.setRate(.18);
+		System.out.println
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
