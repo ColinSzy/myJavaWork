2094a6f6b9d12c31686d057da071d8b76d77238b
Colin <20szymbc@elmbrookstudents.org>
2018-11-05 08:32:48 -0600

    numercount

---
+++ src/loops/LoopOne.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class LoopOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		int number = 0;
+		while (number <= 100)
+		{
+			System.out.println(number);
+			number += 1;
+		}
+		
+		
+	}
+
+}
